09c73edbbc2d77443f174f40b0d99e98b6788d99
amanyKassem <amany.kassem95@gmail.com>
2020-05-27 13:51:05 +0200

    login as visitor

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,8 +10,6 @@ const RootStack = createStackNavigator();
 function renderScreens() {
 	const auth = useSelector(state => state.auth);
 
-	console.log(auth.user , auth)
-
 	if (auth.user !== null && auth.user.success) {
 		return (
 			<RootStack.Screen name={'MainStack'} component={MainStackNavigator}/>
